5f24310514e3d01639aa355f3379c139b8203cc5
Ryan Zhang <zwd071023@outlook.com>
2026-03-02 22:29:51 +0800

    qqmusic

--- qq-music-api.js
+++ qq-music-api.js
@@ -164,6 +164,9 @@ export let searchWithKeyword = async (
               "Sec-Fetch-Mode": "cors",
               "Sec-Fetch-Site": "same-origin",
             },
+            body: '{"comm":{"ct":"19","cv":"1859","uin":"0"},"req":{"method":"DoSearchForQQMusicDesktop","module":"music.search.SearchCgiService"',
+            method: "POST",
+            mode: "cors",
         });
 
         const data = await response.json();
